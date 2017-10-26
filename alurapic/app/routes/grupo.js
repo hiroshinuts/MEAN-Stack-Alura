8ac9b08bdc563dfd4f91579415a8096fe049959e
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2017-10-26 14:10:34 -0200

    Funcionalidade da rota e api do grupo

--- alurapic/app/routes/grupo.js
+++ alurapic/app/routes/grupo.js
@@ -1,14 +1,10 @@
+var api = require('../api/grupo');
+
 module.exports = function(app){
-    
-        app.get('/v1/grupos', function(req, res){
-    
-            var grupos = [
-                {_id: 1, nome: 'esporte'},
-                {_id: 2, nome: 'lugares'},
-                {_id: 3, nome: 'animais'}
-            ];
-    
-            res.json(grupos);
-        });
-    };
-    
+
+    var api = app.api.grupo;
+
+    //passa a função api.lista como parametro
+    app.get('/v1/grupos', api.lista);
+
+};
