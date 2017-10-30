72a3e5f76a644fe3adad7c5dd6fb07f50616775c
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2017-10-30 14:34:52 -0200

    Conexao com MongoDB utilizando Mongoose , Fim cap 9

--- alurapic/server.js
+++ alurapic/server.js
@@ -1,7 +1,8 @@
 var http = require('http');
-var app = require('./config/express')
+var app = require('./config/express');
+require('./config/database')('localhost/alurapic'); // endereço corrigido
 
 http.createServer(app)
 .listen(3000, function() {
-    console.log('Servidor Iniciado');
+    console.log('Servidor iniciado');
 });
